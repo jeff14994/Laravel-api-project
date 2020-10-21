dcb280ecbf48b014d46791d7b122e36fef550ecf
jeff14994 <jeff14994@gmail.com>
2020-10-21 15:41:46 +0800

    Plan to do the CRUD
    CRUD is basically
    1. get all (GET) /api/posts
    2. create post (POST) /api/posts
    3. get a single (GET) /api/posts/{id}
    4. update a single (PUT/PATCH) /api/posts/{id}
    5. delete (DELETE) /api/posts/{id}

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+ //Get the api from http://127.0.0.1:8000/api/testing-the-api
 Route::get('/testing-the-api', function(){
     return ['message' => 'hello'];
 });
+
+//posts
+//CRUD is basically
+//1. get all (GET) /api/posts
+//2. createa post (POST) /api/posts
+//3. get a single (GET) /api/posts/{id}
+//4. update a single (PUT/PATCH) /api/posts/{id}
+//5. delete (DELETE) /api/posts/{id}
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
